b2ca50681c82b8934ee6b8a87c86cb0adab35cfa
Leonardo Welter <leonardo.dias_silva@hotmail.com>
2020-10-14 16:03:33 -0300

    * Instalação e configuração do Bootstrap; * Teste do fortify utilizando templates.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,12 @@
 window._ = require('lodash');
 
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
