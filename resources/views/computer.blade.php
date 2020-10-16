abed3d2ec359458757b7342e339adb4c490e44ed
Leonardo Welter <leonardo.dias_silva@hotmail.com>
2020-10-16 15:36:59 -0300

    * Mockup da recuperação de dados do DB;

--- resources/views/computer.blade.php
+++ resources/views/computer.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+
+<?php
+    // $device = \App\Models\Device::create([
+    //     'serial' => 25700,
+    //     'oem' => 'Dell',
+    //     'model' => 'Heatmaker',
+    //     'cpu' => 'i9 10900°C',
+    //     'ram' => '32GB DDR4',
+    //     'storage' => '1TB 5400 RPM',
+    //     'psu' => '2500W Seasonic',
+    //     'mac' => '01:11:22:AA:BB:CC',
+    //     'name' => 'Shintel goes BRR',
+    //     'os' => 'Windows 10',
+    // ]);
+?>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -10,20 +25,20 @@
                     <table class="table table-striped table-bordered table-responsive-sm">
                         <thead>
                             <tr>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Grupo</th>
-                                <th>Data cadastro</th>
+                                <th>Serial</th>
+                                <th>Marca</th>
+                                <th>Modelo</th>
+                                <th>Name</th>
                                 <th>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach (\App\Models\User::all() as $user) : ?>
+                            <?php foreach (\App\Models\Device::all() as $device) : ?>
                                 <tr>
-                                    <td><?= $user['name'] ?></td>
-                                    <td><?= $user['email'] ?></td>
-                                    <td><?= $user['group'] == 0 ? 'Usuário' : 'Administrador' ?></td>
-                                    <td><?= \Carbon\Carbon::parse($user['created_at'])->format('d/m/Y H:i') ?></td>
+                                    <td><?= $device['serial'] ?></td>
+                                    <td><?= $device['oem'] ?></td>
+                                    <td><?= $device['model'] ?></td>
+                                    <td><?= $device['name'] ?></td>
                                     <td class="text-right">
                                         <a href="#" class="btn btn-sm btn-primary"><i class="fas fa-pen">Editar</i></a>
                                         <a href="#" class="btn btn-sm btn-danger"><i class="fas fa-trash">Excluir</i></a>
--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+
+<?php
+    // $maintenance = \App\Models\Maintenance::create([
+    //     'device_id' => 2,
+    //     'type' => 'Hardware',
+    //     'subtype' => 'Instalação',
+    //     'description' => 'Instalação de um watercooler 360MM',
+    //     'comments' => 'Instalação do watercooler devido a superaquecimento',
+    //     'user_id' => '2',
+    // ]);
+?>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -10,20 +22,26 @@
                     <table class="table table-striped table-bordered table-responsive-sm">
                         <thead>
                             <tr>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Grupo</th>
-                                <th>Data cadastro</th>
+                                <th>Device</th>
+                                <th>Tipo</th>
+                                <th>Subtipo</th>
+                                <th>Descrição</th>
+                                <th>Comentários</th>
+                                <th>Responsável</th>
+                                <th>Data</th>
                                 <th>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach (\App\Models\User::all() as $user) : ?>
+                            <?php foreach (\App\Models\Maintenance::all() as $maintenance) : ?>
                                 <tr>
-                                    <td><?= $user['name'] ?></td>
-                                    <td><?= $user['email'] ?></td>
-                                    <td><?= $user['group'] == 0 ? 'Usuário' : 'Administrador' ?></td>
-                                    <td><?= \Carbon\Carbon::parse($user['created_at'])->format('d/m/Y H:i') ?></td>
+                                    <td><?= App\Models\Device::find($maintenance['device_id'])->serial ?></td>
+                                    <td><?= $maintenance['type'] ?></td>
+                                    <td><?= $maintenance['subtype']  ?></td>
+                                    <td><?= $maintenance['description']  ?></td>
+                                    <td><?= $maintenance['comments']  ?></td>
+                                    <td><?= App\Models\User::find($maintenance['user_id'])->name  ?></td>
+                                    <td><?= \Carbon\Carbon::parse($maintenance['created_at'])->format('d/m/Y H:i') ?></td>
                                     <td class="text-right">
                                         <a href="#" class="btn btn-sm btn-primary"><i class="fas fa-pen">Editar</i></a>
                                         <a href="#" class="btn btn-sm btn-danger"><i class="fas fa-trash">Excluir</i></a>
