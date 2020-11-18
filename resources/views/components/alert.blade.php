65e7f7ecf18f0c21b6201b039059d80e3a9e9142
Leonardo Welter <leonardo.dias_silva@hotmail.com>
2020-11-18 16:38:31 -0300

    * Alteração para exibir erros de validação.

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,20 +1,19 @@
 <div id="alert" class="absolute z-40 top-0 right-0 mt-16 w-56 cursor-pointer" onclick="document.getElementById('alert').hidden = true">
-    <div class="flex text-white rounded shadow-lg p-4 m-2
             @switch($type)
                 @case('error')
-                bg-red-600
+                <div class="flex text-white rounded shadow-lg p-4 m-2 bg-red-600">
                     @break
                 @case('warning')
-                bg-orange-600
+                <div class="flex text-white rounded shadow-lg p-4 m-2 bg-orange-600">
                     @break
                 @case('info')
-                bg-blue-400
+                <div class="flex text-white rounded shadow-lg p-4 m-2 bg-blue-400">
                     @break
                 @case('success')
-                bg-green-400
+                <div class="flex text-white rounded shadow-lg p-4 m-2 bg-green-400">
                     @break
             @endswitch
-                ">
+                
         <div class="align-middle mt-1 text-2xl">
             @switch($type)
                 @case('error')
