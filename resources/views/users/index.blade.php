487f5d450354ef1a38fed930ee5bf1e29eda0f44
Leonardo Welter <leonardo.dias_silva@hotmail.com>
2020-11-27 16:31:55 -0300

    * Fix interface;

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8 pt-6 pb-10 rounded-lg shadow bg-white dark:bg-gray-800">
             <div class="grid grid-cols-4">
                 <span class="mb-4 font-extrabold text-xl col-span-2">Usuários</span>
-                <form class="w-24 md:w-32 max-w-xs col-span-1 md:ml-6 -ml-12">
+                <form class="w-24 md:w-32 max-w-xs col-span-1 md:ml-2 -ml-12">
                     <div class="flex items-center">
                         <input class="rounded-r-none border-r-0 h-10 appearance-none bg-transparent border border-blue-500 rounded dark:border-gray-400 dark:text-gray-200 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="filter" placeholder="Filtrar" aria-label="Filtrar">
                         <button class="rounded-l-none border-l-0 text-xs text-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 dark:text-gray-400 dark:border-gray-400 dark:hover:bg-gray-300 dark:hover:text-gray-700 hover:border-transparent h-10 py-3 px-4 rounded" type="submit">
